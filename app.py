55f95edbb92d62d30a676d2b224e7c5538fceee0
lllGatesJ13 <jacksongates1@icloud.com>
2025-07-07 18:03:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+from flask import Flask, render_template  # Import Flask and render_template function
 
+app = Flask(__name__)  # Create a Flask app instance
+
+@app.route("/")  # Define a route for the homepage ("/")
+def home():
+    # Render the "index.html" template located in the "templates" folder
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
